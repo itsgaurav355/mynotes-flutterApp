e51027736fa53f2b7ab1db9821de99687875bec0
itsgaurav355 <prajapatigaurav355@gmail.com>
2023-05-27 01:01:14 +0530

    step2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -34,20 +35,26 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // final emailVerified = user?.emailVerified ?? false;
-            // if (emailVerified) {
-            // } else {
-            //   return const VerifyEmailView();
-            //Wrong way of Implementing
-            // Navigator.of(context).push(
-            //   MaterialPageRoute(
-            //     builder: (context) => VerifyEmailView(),
-            //   ),
-            // );
-            //}
-            // return const Text("Done");
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is Verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
+          //Wrong way of Implementing
+          // Navigator.of(context).push(
+          //   MaterialPageRoute(
+          //     builder: (context) => VerifyEmailView(),
+          //   ),
+          // );
+          //}
+          // return const Text("Done");
+
           default:
             return const CircularProgressIndicator();
         }
