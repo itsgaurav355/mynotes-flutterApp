9eabba8d88fdb91d20ad99a91b7ca2bd1fe63550
itsgaurav355 <prajapatigaurav355@gmail.com>
2023-05-28 18:46:51 +0530

    step6 Error Handling

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
-import 'package:mynotes/constants/contstants.dart';
+
+import '../constants/routes.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
