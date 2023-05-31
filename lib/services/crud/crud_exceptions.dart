ea20108b5eac754e0c4d1701a0ccb3274699f54b
itsgaurav355 <prajapatigaurav355@gmail.com>
2023-05-31 21:46:50 +0530

    step11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DataBaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CounldNotUpdateNote implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFoundNote implements Exception {}
