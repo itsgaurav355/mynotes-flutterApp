81eadf5fea7e37ef6bfcb0acb3e39b6864e648a3
itsgaurav355 <prajapatigaurav355@gmail.com>
2023-06-05 19:50:45 +0530

    step16

---
+++ lib/views/notes/notes_list_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
+
+import '../../services/crud/notes_service.dart';
+
+typedef DeleteNoteCallback = void Function(DatabaseNote note);
+
+class NotesListView extends StatelessWidget {
+  final List<DatabaseNote> notes;
+  final DeleteNoteCallback onDeleteNote;
+  const NotesListView(
+      {super.key, required this.notes, required this.onDeleteNote});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+        return ListTile(
+          title: Text(
+            note.text,
+            maxLines: 1,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () async {
+              final shouldDelete = await showDeleteDialog(context);
+              if (shouldDelete) {
+                onDeleteNote(note);
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+}
